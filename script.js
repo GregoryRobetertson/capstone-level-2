4d334233c87454b175f543c2f8dbfae50136a064
GregoryRobertson <geefromla@gmail.com>
2024-03-11 18:50:37 -0500

    update file

--- script.js
+++ script.js
@@ -9,21 +9,3 @@ document
     window.location.href = `results.html?city=${encodeURIComponent(city)}`;
   });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const dropdownBtn = document.querySelector(".dropbtn");
-    const dropdownContent = document.querySelector(".dropdown-content");
-
-    dropdownBtn.addEventListener("click", function () {
-      dropdownContent.classList.toggle("show");
-    });
-
-    // Close the dropdown menu if the user clicks outside of it
-    window.addEventListener("click", function (event) {
-      if (!event.target.matches(".dropbtn")) {
-        if (dropdownContent.classList.contains("show")) {
-          dropdownContent.classList.remove("show");
-        }
-      }
-    });
-  });
-
